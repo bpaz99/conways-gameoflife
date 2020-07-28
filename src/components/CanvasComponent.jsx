f8ecd3577fedd06b1468889a64317b1d7ff63a13
MrR3set <bairon.pazrobles@gmail.com>
2020-07-27 22:33:15 -0700

    Canvas component renders CGL Correctly

--- src/components/CanvasComponent.jsx
+++ src/components/CanvasComponent.jsx
@@ -6,7 +6,10 @@ class CanvasComponent extends React.Component {
     super(props)
     this.myRef = React.createRef()
     this.gridSize = this.props.size
-    this.grid = this.Make2Darray()
+    // this.grid = this.Make2Darray()
+    this.state={
+      grid : this.Make2Darray()
+    }
     
   }
   
@@ -19,50 +22,80 @@ class CanvasComponent extends React.Component {
   }
 
   randomfiller(){
-    this.grid.forEach((row,i)=>{
-      row.forEach((col,j)=>{
-        this.grid[i][j] = Math.floor(Math.random()*2)
-      })
-    })
+
+  }
+
+  checkNeighbours(x,y){
+    let total = 0
+    // Counts the elements surrounding the element
+    for(let i=-1;i<=1;i++)
+      for(let j=-1;j<=1;j++)
+        total += this.state.grid[x-i][y-j]
+    total -= this.state.grid[x][y] //Substract the current element as the loop counts it as well
+    return total
   }
 
   Sketch = (p) => {
-    // let cellSize = this.props.width/this.gridSize
     let cellSize = this.props.width/this.gridSize
-    console.log("CELL SIZE",cellSize)
     p.setup = () => {
-      p.createCanvas(this.props.width,this.props.height)
+      p.createCanvas(this.props.width,this.props.width)
+      this.state.grid.forEach((row,i)=>{
+        row.forEach((col,j)=>{
+          this.state.grid[i][j] = p.floor(p.random(2))
+        })
+      })
     }
 
     p.draw = () => {
       p.background(0)
-      //Create cells
-      this.grid.forEach((row,i)=>{
+      //Draw cells
+      this.state.grid.forEach((row,i)=>{
         row.forEach((col,j)=>{
-
-          console.log(this.grid[i][j])
-
-          if(this.grid[i][j] === 1){
+          if(this.state.grid[i][j] === 1){
             p.fill(255)
             p.rect(i*cellSize,j*cellSize,cellSize-1,cellSize-1)
           }
-
         })
       })
 
-
+      //Check next generation
+      let nextGeneration = this.Make2Darray()
+      this.state.grid.forEach((row,i)=>{
+        row.forEach((col,j)=>{
+          if (i===0||i===this.gridSize-1||j===0||j===this.gridSize-1){
+            nextGeneration[i][j]=this.state.grid[i][j]
+          }else{
+            let neighbours = this.checkNeighbours(i,j)
+            if(this.state.grid[i][j] === 0 && neighbours===3 ){
+              nextGeneration[i][j] = 1
+            }else if(this.state.grid[i][j] === 1 &&( neighbours<2 ||neighbours>3)){
+              nextGeneration[i][j]=0
+            }else{
+              nextGeneration[i][j]=this.state.grid[i][j]
+            }
+          }
+        })
+      })
+      p.noLoop()
+      if(JSON.stringify(nextGeneration)===JSON.stringify(this.state.grid)){
+        console.log("Generation Ended")
+        this.props.setFinished()
+      }else{
+        setTimeout(()=>{
+          this.setState({grid:nextGeneration})
+          this.props.addGeneration()
+          p.loop()
+        },50)
+      }
     }
   }
+
 
   componentDidMount(){
     this.myP5 = new p5(this.Sketch,this.myRef.current)
   }
 
   render(){
-    console.log("Grid",this.grid)
-    this.randomfiller()
-
-    console.log(this.props.width)
     return (<div ref={this.myRef}></div>)
   }
 }
---
+++ src/components/Information.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Information() {
+  return (
+    <div className="info-wrapper">
+        <div className="content">
+            <h1>About this algorithm</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat vero nam vel quasi provident minima similique asperiores. Praesentium illum voluptatibus dolor odit, mollitia a nemo cumque necessitatibus iure inventore dolore quis provident temporibus architecto, quisquam libero enim nam alias tempora nulla illo soluta voluptas sit magnam. Eveniet ea magni dignissimos.</p>
+        </div>
+    </div>
+  );
+}
+
+export default Information;
