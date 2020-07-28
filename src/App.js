8fd4059840af9c49a798742191c31775964e8745
MrR3set <bairon.pazrobles@gmail.com>
2020-07-27 20:43:31 -0700

    Set up component hierarchy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Dashboard from "./components/Dashboard"
+import Information from "./components/Information"
+import CanvasComponent from "./components/CanvasComponent"
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title">
+        <h1>Conway's game of life</h1>
+      </div>
+      <Dashboard></Dashboard>
+      <Information></Information>
+
+      {/* <CanvasComponent size={25} width={800} height={600}/> */}
+
+
+
     </div>
   );
 }
