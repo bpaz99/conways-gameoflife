d8e111e4ef6e18a0c11cf8d9ee605399ee23ab3f
MrR3set <bairon.pazrobles@gmail.com>
2020-07-29 23:23:14 -0700

    Added background simulation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Dashboard from "./components/Dashboard"
+import Background from "./components/Background"
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
+      <Background></Background>
       <div className="title">
         <h1>Conway's game of life</h1>
       </div>
